d645e8880c5045bdad6ea6956db8ebc4b3080161
learndataa <63557075+learndataa@users.noreply.github.com>
2023-04-07 23:24:54 +0530

    udpate to site
    update to site

---
+++ README.md
@@ -0,0 +1,1 @@
+# nileshingle.github.io
---
+++ javascripts/effects.js
@@ -0,0 +1,11 @@
+
+  <script type="text/javascript" src="http://alexgorbatchev.com/pub/sh/2.1.364/scripts/shCore.js"></script>
+  <script type="text/javascript" src="http://alexgorbatchev.com/pub/sh/2.1.364/scripts/shBrushRuby.js"></script> <script type="text/javascript" src="http://alexgorbatchev.com/pub/sh/2.1.364/scripts/shLegacy.js"></script>
+  <link type="text/css" rel="stylesheet" href="http://alexgorbatchev.com/pub/sh/2.1.364/styles/shCore.css" />
+  <link type="text/css" rel="stylesheet" href="http://alexgorbatchev.com/pub/sh/2.1.364/styles/shThemeDefault.css" />     <script language="javascript">
+  window.onload = function () {
+      dp.SyntaxHighlighter.ClipboardSwf = 'http://alexgorbatchev.com/pub/sh/2.1.364/scripts/clipboard.swf';
+      dp.SyntaxHighlighter.HighlightAll('code');
+      dp.SyntaxHighlighter.BloggerMode();
+  }
+  </script>
